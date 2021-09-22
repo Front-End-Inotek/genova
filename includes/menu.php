3087b59fc9bce7eb049d008cb5c4cad38da1bd13
it <it@inotek.mx>
2021-09-22 17:10:31 -0500

    Generar menu lateral

--- includes/menu.php
+++ includes/menu.php
@@ -7,59 +7,143 @@
   $usuario->datos($_GET['id']);
   $tiempo=time();
   if($_GET['token']== $usuario->token & $usuario->fecha_vencimiento>=$tiempo & $usuario->activo=1 ){
-  echo '
-  <div id="sideNavigation" class="sidenav">
-    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-    <a href="#">Reservaciones</a>
-    <li class="nav-item">
-                    <a class="nav-link collapsed text-truncate" href="#submenu1" data-toggle="collapse" data-target="#submenu1"><i class="fa fa-table"></i> <span class="d-none d-sm-inline">Reports</span></a>
-                    <div class="collapse" id="submenu1" aria-expanded="false">
-                        <ul class="flex-column pl-2 nav">
-                            <li class="nav-item"><a class="nav-link py-0" href="#"><span>Orders</span></a></li>
+    echo '
+    <div id="sideNavigation" class="sidenav">
+      <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+      <a href="#">Reservaciones</a>
+          <li class="nav-item">';
+          
+          //$permisos_habitaciones=$usuario->usuario_ver+$usuario->usuario_agregar;  AQUI ES NORMAL SIN SUBMENU
+          /*$permisos_habitaciones=1;
+          if($permisos_habitaciones>0){
+            echo '
+            <a class="nav-link collapsed text-truncate" href="#submenu1" data-toggle="collapse" data-target="#submenu1"><i class="fa fa-table"></i> <span class="d-none d-sm-inline">Habitaciones</span></a>
+              <div class="collapse" id="submenu1" aria-expanded="false">
+                <ul class="flex-column pl-2 nav">';
+                if($usuario->usuario_ver==1){
+                  echo '
+                    <li class="nav-item"><a class="nav-link py-0" href="#" onclick="agregar_tipo()"><span>Tipo</span></a></li>';
+                }
+                if($usuario->usuario_ver==1){
+                  echo '
+                    <li class="nav-item"><a class="nav-link py-0" href="#"><span>Tarifas</span></a></li>';
+                }
+                if($usuario->usuario_ver==1){
+                  echo '
+                    <li class="nav-item"><a class="nav-link py-0" href="#"><span>Habitacion</span></a></li>';
+                }
+          }*/
+     
+                  $permisos_habitaciones=1;
+                  if($permisos_habitaciones>0){
+                    echo '
+                    <a class="nav-link collapsed text-truncate" href="#submenu1" data-toggle="collapse" data-target="#submenu1"><i class="fa fa-table"></i> <span class="d-none d-sm-inline">Habitaciones</span></a>
+                      <div class="collapse" id="submenu1" aria-expanded="false">
+                        <ul class="flex-column pl-2 nav">';
+
+                          if($permisos_habitaciones>0){
+                            echo '
                             <li class="nav-item">
-                                <a class="nav-link  text-truncate collapsed py-1" href="#submenu1sub1" data-toggle="collapse" data-target="#submenu1sub1"><span>Habitaciones</span></a>
+                                <a class="nav-link  text-truncate collapsed py-1" href="#submenu1sub1" data-toggle="collapse" data-target="#submenu1sub1"><span>Tipo</span></a>
                                 <div class="collapse" id="submenu1sub1" aria-expanded="false">
-                                    <ul class="flex-column nav pl-4">
-                                        <li class="nav-item">
-                                            <a class="nav-link p-1 text-truncate" href="#">
-                                                <i class="fa fa-fw fa-clock-o"></i> Daily </a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link p-1 text-truncate" href="#">
-                                                <i class="fa fa-fw fa-dashboard"></i> Dashboard </a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link p-1 text-truncate" href="#">
-                                                <i class="fa fa-fw fa-bar-chart"></i> Charts </a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link p-1 text-truncate" href="#">
-                                                <i class="fa fa-fw fa-compass"></i> Areas </a>
-                                        </li>
+                                    <ul class="flex-column nav pl-4">';
+                                        if($usuario->usuario_ver==1){
+                                          echo '
+                                          <li class="nav-item">
+                                              <a class="nav-link p-1 text-truncate" href="#" onclick="agregar_tipos()">
+                                                  <i class="fa fa-fw fa-clock-o"></i> &nbsp; Ver Tipos</a>
+                                          </li>';
+                                        }
+                                        if($usuario->usuario_ver==1){
+                                          echo '
+                                          <li class="nav-item">
+                                              <a class="nav-link p-1 text-truncate" href="#">
+                                                  <i class="fa fa-fw fa-dashboard"></i> &nbsp; Agregar Tipo</a>
+                                          </li>';
+                                        }
+                                        echo '
                                     </ul>
                                 </div>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-    <a href="#">Habitaciones</a>
-    <a href="#">Clientes</a>
-  </div>
+                            </li>'; 
+                          }
 
-  <nav class="topnav">
-    <a href="#" onclick="openNav()">
-      <svg width="30" height="30" id="icoOpen">
-          <path d="M0,5 30,5" stroke="#000" stroke-width="5"/>
-          <path d="M0,14 30,14" stroke="#000" stroke-width="5"/>
-          <path d="M0,23 30,23" stroke="#000" stroke-width="5"/>
-      </svg>
-    </a>
-  </nav>
-  
-  <div id="main">
-  <!-- Add all your websites page content here  -->
-  </div>
-  ';
+                          $permisos_habitaciones=1;
+                          if($permisos_habitaciones>0){
+                            echo '
+                            <li class="nav-item">
+                                <a class="nav-link  text-truncate collapsed py-1" href="#submenu1sub2" data-toggle="collapse" data-target="#submenu1sub2"><span>Tarifa</span></a>
+                                <div class="collapse" id="submenu1sub2" aria-expanded="false">
+                                    <ul class="flex-column nav pl-4">';
+                                        if($usuario->usuario_ver==1){
+                                          echo '
+                                          <li class="nav-item">
+                                              <a class="nav-link p-1 text-truncate" href="#">
+                                                  <i class="fa fa-fw fa-clock-o"></i> &nbsp; Ver Tarifas</a>
+                                          </li>';
+                                        }
+                                        if($usuario->usuario_ver==1){
+                                          echo '
+                                          <li class="nav-item">
+                                              <a class="nav-link p-1 text-truncate" href="#">
+                                                  <i class="fa fa-fw fa-dashboard"></i> &nbsp; Agregar Tarifa</a>
+                                          </li>';
+                                        }
+                                        echo '
+                                    </ul>
+                                </div>
+                            </li>'; 
+                          }
+
+                          $permisos_habitaciones=1;
+                          if($permisos_habitaciones>0){
+                            echo '
+                            <li class="nav-item">
+                                <a class="nav-link  text-truncate collapsed py-1" href="#submenu1sub3" data-toggle="collapse" data-target="#submenu1sub3"><span>Habitacion</span></a>
+                                <div class="collapse" id="submenu1sub3" aria-expanded="false">
+                                    <ul class="flex-column nav pl-4">';
+                                        if($usuario->usuario_ver==1){
+                                          echo '
+                                          <li class="nav-item">
+                                              <a class="nav-link p-1 text-truncate" href="#">
+                                                  <i class="fa fa-fw fa-clock-o"></i> &nbsp; Ver Habitaciones</a>
+                                          </li>';
+                                        }
+                                        if($usuario->usuario_ver==1){
+                                          echo '
+                                          <li class="nav-item">
+                                              <a class="nav-link p-1 text-truncate" href="#">
+                                                  <i class="fa fa-fw fa-dashboard"></i> &nbsp; Agregar Habitacion</a>
+                                          </li>';
+                                        }
+                                        echo '
+                                    </ul>
+                                </div>
+                            </li>'; 
+                          }
+                  }
+                  
+                echo ' 
+                </ul>
+              </div>
+          </li>
+      <a href="#">Reportes</a>
+      <a href="#">Clientes</a>
+    </div>
+
+    <nav class="topnav">
+      <a href="#" onclick="openNav()">
+        <svg width="30" height="30" id="icoOpen">
+            <path d="M0,5 30,5" stroke="#000" stroke-width="5"/>
+            <path d="M0,14 30,14" stroke="#000" stroke-width="5"/>
+            <path d="M0,23 30,23" stroke="#000" stroke-width="5"/>
+        </svg>
+      </a>
+    </nav>
+    
+    <div id="main">
+    <!-- Add all your websites page content here  -->
+    </div>
+    ';
   }else{
     echo 'Su sesion a espirado o su cuenta ha sido abierta desde otro dispositivo , es necesario iniciar sesion nuevamente ';
     echo "<script>";
--- js/events.js
+++ js/events.js
@@ -132,10 +132,173 @@ function closeNav() {
     document.getElementById("main").style.marginLeft = "0";
 }
 
-// Agregar un usuario
-function agregar_usuarios(id){
+//Habitaciones
+
+// Agregar un tipo
+function agregar_tipos(id){
 	$('#area_trabajo').hide();
 	$('#area_trabajo_menu').show();
-	$("#area_trabajo_menu").load("includes/agregar_usuarios.php?id="+id); 
+	$("#area_trabajo_menu").load("includes/agregar_tipos.php?id="+id); 
     $(".navbar-collapse").collapse('hide');
+}
+
+// Guardar una herramienta
+function guardar_herramienta(){
+    var id=localStorage.getItem("id");
+	var nombre= encodeURI(document.getElementById("nombre").value);
+    var marca= encodeURI(document.getElementById("marca").value);
+	var modelo= encodeURI(document.getElementById("modelo").value);
+	var descripcion= encodeURI(document.getElementById("descripcion").value);
+    var cantidad_almacen= document.getElementById("cantidad_almacen").value;
+    var cantidad_prestadas= document.getElementById("cantidad_prestadas").value;
+	
+
+	if(nombre.length >0 && marca.length >0 && modelo.length >0  && cantidad_almacen >=0 && cantidad_prestadas >=0){
+			//$('#boton_herramienta').hide();
+			$("#boton_herramienta").html('<div class="spinner-border text-primary"></div>');
+			var datos = {
+				  "nombre": nombre,
+				  "marca": marca,
+                  "modelo": modelo,
+			      "descripcion": descripcion,
+                  "cantidad_almacen": cantidad_almacen,
+                  "cantidad_prestadas": cantidad_prestadas,
+                  "id": id,
+				};
+			$.ajax({
+				  async:true,
+				  type: "POST",
+				  dataType: "html",
+				  contentType: "application/x-www-form-urlencoded",
+				  url:"includes/guardar_herramienta.php",
+				  data:datos,
+				  beforeSend:loaderbar,
+				  success:ver_herramientas,
+				  timeout:5000,
+				  error:problemas_sistema
+				});
+				return false;
+			}else{
+				alert("Campos incompletos");
+			}
+}
+
+// Muestra las herramientas de la bd
+function ver_herramientas(){
+    var id=localStorage.getItem("id");
+	$('#area_trabajo').hide();
+	$('#area_trabajo_menu').show();
+	$("#area_trabajo_menu").load("includes/ver_herramientas.php?id="+id);
+    $(".navbar-collapse").collapse('hide');
+}
+
+// Muestra la paginacion de las herramientas
+function ver_herramientas_paginacion(buton,posicion){
+    //alert(id);
+    var id=localStorage.getItem("id");
+    $("#paginacion_herramientas").load("includes/ver_herramientas_paginacion.php?posicion="+posicion+"&id="+id);   
+}
+
+// Barra de diferentes busquedas en ver herramientas
+function buscar_herramienta(){
+    var a_buscar=encodeURIComponent($("#a_buscar").val());
+    var id=localStorage.getItem("id");
+    if(a_buscar.length >0){
+        $('.pagination').hide();
+    }else{
+        $('.pagination').show();
+    }
+	$("#tabla_herramienta").load("includes/buscar_herramienta.php?a_buscar="+a_buscar+"&id="+id);  
+}
+
+// Editar una herramienta
+function editar_herramienta(id){
+    $("#area_trabajo_menu").load("includes/editar_herramienta.php?id="+id);
+}
+
+// Editar una herramienta
+function modificar_herramienta(id){
+    var herramienta_id=localStorage.getItem("id");
+	var nombre= encodeURI(document.getElementById("nombre").value);
+    var marca= encodeURI(document.getElementById("marca").value);
+    var modelo= encodeURI(document.getElementById("modelo").value);
+	var descripcion= encodeURI(document.getElementById("descripcion").value);
+    var cantidad_almacen= document.getElementById("cantidad_almacen").value;
+    var cantidad_prestadas= document.getElementById("cantidad_prestadas").value;
+   
+
+    if(id >0){
+        //$('#boton_herramienta').hide();
+			$("#boton_herramienta").html('<div class="spinner-border text-primary"></div>');
+        var datos = {
+              "id": id,
+              "nombre": nombre,
+			  "marca": marca,
+              "modelo": modelo,
+			  "descripcion": descripcion,
+              "cantidad_almacen": cantidad_almacen,
+              "cantidad_prestadas": cantidad_prestadas,
+              "herramienta_id": herramienta_id,
+            };
+        $.ajax({
+              async:true,
+              type: "POST",
+              dataType: "html",
+              contentType: "application/x-www-form-urlencoded",
+              url:"includes/aplicar_editar_herramienta.php",
+              data:datos,
+              //beforeSend:loaderbar,
+              success:ver_herramientas,
+              //success:problemas_sistema,
+              timeout:5000,
+              error:problemas_sistema
+            });
+        return false;
+    }else{
+        alert("Campos incompletos");
+    }    
+}
+
+// Generar reporte de herramienta
+function reporte_herramienta(){
+	var id=localStorage.getItem("id");
+    window.open("includes/reporte_herramienta.php?id="+id);
+}
+
+// Borrar una herramienta
+function borrar_herramienta(id){
+    var herramienta_id=localStorage.getItem("id");
+    $('#caja_herramientas').modal('hide');
+    if (id >0) {
+        var datos = {
+                "id": id,
+                "herramienta_id": herramienta_id,
+            };
+        $.ajax({
+                async:true,
+                type: "POST",
+                dataType: "html",
+                contentType: "application/x-www-form-urlencoded",
+                url:"includes/borrar_herramienta.php",
+                data:datos,
+                beforeSend:loaderbar,
+                success:ver_herramientas,
+                timeout:5000,
+                error:problemas_sistema
+            });
+        return false;
+    }
+}
+
+// Modal de borrar herramienta
+function aceptar_borrar_herramienta(id){
+	$("#mostrar_herramientas").load("includes/borrar_modal_herramienta.php?id="+id);
+}
+
+// Regresar a la pagina anterior de editar herramienta
+function regresar_editar_herramienta(){
+    var id=localStorage.getItem("id");
+    $('#area_trabajo').hide();
+	$('#area_trabajo_menu').show();
+    $("#area_trabajo_menu").load("includes/ver_herramientas.php?id="+id);
 }
