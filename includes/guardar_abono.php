33d505893e5b446d61df1fd270e1ab2d1ed6167d
it <it@inotek.mx>
2021-11-17 17:54:06 -0600

    Generar estado de cuenta

---
+++ includes/guardar_abono.php
@@ -0,0 +1,15 @@
+<?php
+  date_default_timezone_set('America/Mexico_City');
+  include_once("clase_cuenta.php");
+  include_once("clase_hab.php");
+  include_once("clase_log.php");//(hab_id,estado){
+  $cuenta= NEW Cuenta(0);
+  $hab= NEW Hab($_POST['hab_id']);
+  $logs = NEW Log(0);
+  $mov= $hab->mov;
+  $nombre= $hab->nombre;
+  $cuenta->guardar_cuenta($_POST['usuario_id'],$mov,urldecode($_POST['descripcion']),$_POST['forma_pago'],$_POST['faltante'],$_POST['abono']);
+  $logs->guardar_log($_POST['usuario_id'],"Agregar abono a la habitacion: ". $nombre);
+  echo $_POST['hab_id']."/".$_POST['estado'];
+?>
+
