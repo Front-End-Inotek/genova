f196035d8bf235c9fe1203ddfab2e36be192c5a5
it <it@inotek.mx>
2021-11-23 11:10:42 -0600

    Boton para unificar las cuentas al seleccionar una habitacion en especifico

---
+++ includes/modal_unificar_cuentas.php
@@ -0,0 +1,28 @@
+<?php
+  date_default_timezone_set('America/Mexico_City');
+  include_once("clase_hab.php");
+  $hab = NEW Hab(0);
+
+  echo '
+  <!-- Modal content-->
+  <div class="modal-content">
+    <div class="modal-header">';
+      if($_GET['monto'] == 1){
+        echo 'Seleccionar cuarto para cambiar cargo $'.number_format($_GET['cargo'], 2).'';
+      }else{
+        echo 'Seleccionar cuarto para cambiar abono $'.number_format($_GET['cargo'], 2).'';
+      }
+      echo '<button type="button" class="close" data-dismiss="modal">&times;</button>
+    </div><br>
+
+    <div class="modal-body">';
+      echo '<div class="row">';
+        $hab->cambiar_hab_ocupada($_GET['monto'],$_GET['id'],$_GET['hab_id'],$_GET['estado']); 
+      echo '</div>
+    <div>     
+
+    <div class="modal-footer" id="boton_abono">
+      <button type="button" class="btn btn-danger" data-dismiss="modal"> Cancelar</button>
+    </div>
+  </div>';
+?>
