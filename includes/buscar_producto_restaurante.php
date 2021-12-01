2cf5611ff748782da125f69a8e6b37c0a0ce1465
it <it@inotek.mx>
2021-12-01 13:56:51 -0600

    Vistas cuadradas de todos los botones junto con rendondear sus bordes

---
+++ includes/buscar_producto_restaurante.php
@@ -0,0 +1,8 @@
+<?php
+	date_default_timezone_set('America/Mexico_City');
+  include_once("clase_inventario.php");
+  $inventario= NEW Inventario(0);
+  if(strlen ($_GET['a_buscar'])>0){
+    $inventario->mostar_producto_busqueda(urldecode($_GET['a_buscar']),$_GET['hab_id'],$_GET['estado'],$_GET['mov']);
+  }
+?>
