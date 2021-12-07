f7a1f79732941bf271d6543fe56e0bd06afa64a5
it <it@inotek.mx>
2021-12-07 16:40:45 -0600

    Generar estado 2 de sucio y mandar a terminar en una habitacion

---
+++ includes/hab_terminar.php
@@ -0,0 +1,11 @@
+<?php
+	include_once("clase_movimiento.php");
+  include_once("clase_hab.php");
+  include_once('clase_log.php');
+  $movimiento = NEW Movimiento(0);
+  $hab = NEW Hab($_POST['hab_id']);
+  $logs = NEW Log(0);
+  $mov->terminar_mov($hab->mov);
+  $hab->cambiohab($_POST['hab_id'],$hab->mov,2);
+  $logs->guardar_log($_POST['usuario_id'],"Terminar hospedaje en habitaciona: ". $hab->nombre);
+?>
