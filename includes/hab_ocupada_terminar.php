911d8388df60c8d493fc4ad4eb3769df50e27a49
it <it@inotek.mx>
2021-12-08 15:48:56 -0600

    Subestados en estado 1 ocupado

---
+++ includes/hab_ocupada_terminar.php
@@ -0,0 +1,13 @@
+<?php
+	include_once("clase_movimiento.php");
+  include_once("clase_hab.php");
+  include_once('clase_log.php');
+  $movimiento = NEW Movimiento(0);
+  $hab = NEW Hab($_POST['hab_id']);
+  $logs = NEW Log(0);
+  $estado_interno= $movimiento->mostrar_estado_interno($hab->mov);
+  $movimiento->editar_detalle_fin($hab->mov);
+  $movimiento->editar_estado_interno($hab->mov,1.1);
+  $logs->guardar_log($_POST['usuario_id'],"Habitacion ocupada sucia: ". $hab->nombre);
+  $logs->guardar_log($_POST['usuario_id'],"Terminar estado interno .$estado_interno. de la habitación: ". $hab->nombre);
+?>
