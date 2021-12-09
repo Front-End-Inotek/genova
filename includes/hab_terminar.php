60f4fab32230c733c128e688359ee7bbe9d94a25
it <it@inotek.mx>
2021-12-08 18:03:44 -0600

    Subestados en estado 2 sucia

---
+++ includes/hab_terminar.php
@@ -0,0 +1,12 @@
+<?php
+	include_once("clase_movimiento.php");
+  include_once("clase_hab.php");
+  include_once('clase_log.php');
+  $movimiento = NEW Movimiento(0);
+  $hab = NEW Hab($_POST['hab_id']);
+  $logs = NEW Log(0);
+  $estado_interno= $movimiento->mostrar_estado_interno($hab->mov);
+  $movimiento->editar_detalle_fin($hab->mov);
+  $movimiento->editar_estado_interno($hab->mov,0);
+  $logs->guardar_log($_POST['usuario_id'],"Terminar estado interno $estado_interno de la habitacion: ". $hab->nombre);
+?>
