2e69dcbaafe4d84cfa0b7b014b14c33339fd17f2
it <it@inotek.mx>
2022-01-04 15:18:58 -0600

    Cambiar los colores de los estados de los racks

---
+++ includes/agregar_archivo.php
@@ -0,0 +1,50 @@
+<?php
+  date_default_timezone_set('America/Mexico_City');
+  include_once('clase_configuracion.php');
+  $config = NEW Configuracion();
+  
+  echo ' <div class="container blanco"> 
+          <div class="col-sm-12 text-left"><h2 class="text-dark margen-1">CAMBIAR COLOR</h2></div>';
+          echo '
+          <form enctype="multipart/form-data" action="includes/guardar_archivo.php?usuario_id='.$_GET['usuario_id'].'" method="POST"> 
+                <div class="row"> 
+                        <div class="col-sm-2"><label for="principal">Selecciona estado</label>:</div>
+                        <div class="col-sm-2">
+                                <div class="form-group">
+                                        <select class="form-control" id="estado" name="estado">
+                                        <option value="estado0">Disponible</option>
+                                        <option value="estado1">Ocupada</option>
+                                        <option value="estado2">Sucia</option>
+                                        <option value="estado3">Limpieza</option>
+                                        <option value="estado4">Mant.</option>
+                                        <option value="estado5">Super.</option>
+                                        <option value="estado6">Cancelada</option>
+                                        </select>
+                                </div>
+                        </div>
+                        <div class="col-sm-2"></div>
+                        <div class="col-sm-2"><label for="principal">Color Principal:</label></div>
+                        <div class="col-sm-4">
+                                <input type="color" value="#7b2416" id="principal" name="principal">
+                        </div>
+                </div>
+
+                <div class="row"> 
+                        <div class="col-sm-2"><label for="fondo">Color Fondo:</label></div>
+                        <div class="col-sm-4">
+                                <input type="color" value="#c03a2f" id="fondo" name="fondo">
+                        </div>
+                        <div class="col-sm-2"><label for="letra">Color Letra:</label></div>
+                        <div class="col-sm-2">
+                                <input type="color" value="#D98880" id="letra" name="letra">
+                        </div>
+                        <div class="col-sm-2">
+                        <div id="boton_tipo">
+                                <input type="submit" class="btn btn-success btn-block" value="Guardar">
+                        </div>
+                        </div>
+                </div>
+          </form>
+  </div>';
+  //#ff0000
+?>
---
+++ includes/guardar_archivo.php
@@ -0,0 +1,51 @@
+<?php 
+include_once('clase_log.php');
+$logs = NEW Log(0);
+
+// Se cargan los datos necesarios
+//$asu=$_REQUEST['asunto'];
+$estado=$_REQUEST['estado'];
+$principal=$_REQUEST['principal'];
+$fondo=$_REQUEST['fondo'];
+$letra=$_REQUEST['letra'];
+
+// Se comprueba si existe el archivo previamente antes de generarlo
+if(!file_exists('../styles/'.$estado.'.css')){
+  //echo "Existio un problema borrando el archivo";
+}else{
+  //echo "Archivo borrado con exito";
+  unlink('../styles/'.$estado.'.css');
+}
+// Se genera el archivo de css
+$archivo=fopen('../styles/'.$estado.'.css','a') or die ('Error al crear');
+
+fwrite($archivo,'.'.$estado.'{');
+fwrite($archivo,"\n");
+fwrite($archivo,'background-color:'.$principal.';');
+fwrite($archivo,"\n");
+fwrite($archivo,'color:'.$letra.';');
+fwrite($archivo,"\n");
+fwrite($archivo,'padding: 0em 10px 0px 10px;');
+fwrite($archivo,"\n");
+fwrite($archivo,'border-radius: 10px;');
+fwrite($archivo,"\n");
+fwrite($archivo,'}');
+fwrite($archivo,"\n");
+
+fwrite($archivo,'.'.$estado.':hover{');
+fwrite($archivo,"\n");
+fwrite($archivo,'background-color:'.$fondo.';');
+fwrite($archivo,"\n");
+fwrite($archivo,'color:'.$letra.';');
+fwrite($archivo,"\n");
+fwrite($archivo,'padding: 0em 10px 0px 10px;');
+fwrite($archivo,"\n");
+fwrite($archivo,'border-radius: 10px;');
+fwrite($archivo,"\n");
+fwrite($archivo,'}');
+
+
+echo "Se creo correctamente el archivo";
+$logs->guardar_log($_GET['usuario_id'],"Cambiar los colores del ". $estado);
+header("location:../inicio.php");
+?>
