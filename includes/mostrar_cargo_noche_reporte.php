c96531500b79a6f47b7d971d0e961739bd76c6ca
it <it@inotek.mx>
2022-03-15 11:39:15 -0600

    Reporte de corte con su respectiva vista de ver cortes

---
+++ includes/mostrar_cargo_noche_reporte.php
@@ -0,0 +1,16 @@
+<?php
+  date_default_timezone_set('America/Mexico_City');
+  include_once('clase_cargo_noche.php');
+  $cargo_noche = NEW Cargo_noche(0);
+	
+  $numero_actual= $cargo_noche->ultima_insercion();
+  //$numero_actual= 19;
+  $ancho= $_GET['ancho']-50;
+  $alto= $_GET['alto']-50;
+  if($numero_actual > 0){
+    echo '<object width="'.$ancho.'" height="'.$alto.'" type="application/pdf" data="../reportes/reservaciones/cargo_noche/reporte_cargo_noche_'.$numero_actual.'.pdf">
+		<param name="src" value="//192.168.1.145/gestion/viewer/doc.pdf" />
+		<p>No PDF available</p>
+		</object>';
+  }
+?>
