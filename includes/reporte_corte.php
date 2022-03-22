b3298c877abe25438f9a74f067d6c0c419e7fbe4
it <it@inotek.mx>
2022-03-22 15:31:26 -0600

    Cambio de orden y ajustes en reporte de corte

--- includes/reporte_corte.php
+++ includes/reporte_corte.php
@@ -264,16 +264,13 @@
 
   $corte_id= $corte->ultima_insercion();
   // Cambiar concepto a inactivo
-  //$concepto->cambiar_activo($_GET['usuario_id']);
+  $concepto->cambiar_activo($_GET['usuario_id']);
   // Cambiar ticket a estado 1 (en corte) y poner el corte que le corresponde
-  //$ticket->editar_estado($_GET['usuario_id'],$corte_id,1);
-  $pdf->Ln(56);
-  for($i=1;$i<=37;$i++)
-    $pdf->Cell(0,10,'Imprimiendo linea numero '.$i,0,1);
+  $ticket->editar_estado($_GET['usuario_id'],$corte_id,1);
   
   $logs->guardar_log($_GET['usuario_id'],"Reporte corte con etiqueta: ".$nueva_etiqueta.' del '.$dia.' de '.$mes.' de '.$anio); 
   //$pdf->Output("reporte_corte.pdf","I");// I muestra y F descarga con directorio y D descarga en descargas
-  $pdf->Output("../reportes/corte/reporte_corte_".$nueva_etiqueta.".pdf","I");
+  $pdf->Output("../reportes/corte/reporte_corte_".$nueva_etiqueta.".pdf","F");
   //$pdf->Output("../reportes/reservaciones/cargo_noche/reporte_corte.pdf","I");
       //echo 'Reporte corte';*/
 ?>
