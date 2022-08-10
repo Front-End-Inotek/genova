d36a823afc39819afdbe1bb4c2213af18da1007b
it <it@inotek.mx>
2022-08-10 09:35:08 -0500

    Retoques globales de reportes y corte

--- includes/datos_servidor.php
+++ includes/datos_servidor.php
@@ -2,7 +2,7 @@
   //En este archivo se almacenaran los datos del servidor para realilzar la conexion
   date_default_timezone_set('America/Mexico_City');
   $servidor="localhost";
-  $usuario_servidor="root";
+  $usuario_servidor="kala";
   $password="ORWare1986";
   $base_datos="visit";
 ?>
