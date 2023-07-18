61554b43d2753bb7fed5ba3b8d3ec115e54ac70e
josueG <dev_j@inotek.mx>
2023-07-18 08:56:24 -0600

    fix: (rack y ed.cuenta) cambio de texto y correxion de estilos

--- includes/agregar_cargo.php
+++ includes/agregar_cargo.php
@@ -29,7 +29,7 @@
         echo '<h3 class="modal-title">AHCloud>Panel>Cuenta Maestra '.$cm->nombre.'</h3>';
       }
     echo '
-      <h2>-Cargar</h2>
+      <h2>Cobrar</h2>
       <button type="button" class="close" data-dismiss="modal">&times;</button>
     </div><br>
     <div class="modal-body">
