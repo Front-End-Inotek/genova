cd6c4c80b60fe898559348a50df2595fbb7ff835
josueG <dev_j@inotek.mx>
2023-07-26 11:23:37 -0600

    fix diseño respnsive

--- includes/agregar_datos_vehiculo.php
+++ includes/agregar_datos_vehiculo.php
@@ -43,93 +43,93 @@ if(sizeof($datos_vehiculo)>0) {
         </div>
         <div class="modal-body">
 
-        <div class="row">
-        <div class="col-6">
+        <div class="row flex-wrap">
+        <div class="col-12 col-sm-6">
         <div class="input-group mb-3">
             <div class="input-group-prepend">
-                <span class="input-group-text" id="inputGroup-sizing-default"  style="width: 130px; font-size: 12px;"> Matrícula </span>
+                <span class="input-group-text" id="inputGroup-sizing-default"  > Matrícula </span>
             </div>
-            <input value="'.$matricula.'" required type="text" id="matricula" name ="matricula" class="form-control" aria-label="Default" autocomplete="off" aria-describedby="inputGroup-sizing-default" style="font-size: 14px;" >
+            <input value="'.$matricula.'" required type="text" id="matricula" name ="matricula" class="form-control" aria-label="Default" autocomplete="off" aria-describedby="inputGroup-sizing-default" >
         </div>
         </div>
 
-        <div class="col-6">
+        <div class="col-12 col-sm-6">
         <div class="input-group mb-3">
             <div class="input-group-prepend">
-                <span class="input-group-text" id="inputGroup-sizing-default"  style="width: 130px; font-size: 12px;"> Marca </span>
+                <span class="input-group-text" id="inputGroup-sizing-default"> Marca </span>
             </div>
-            <input value="'.$marca.'" required type="text" id="marca" name="marca" class="form-control" aria-label="Default" autocomplete="off" aria-describedby="inputGroup-sizing-default" style="font-size: 14px;" >
-        </div>
-        </div>
-        </div>
-
-        <div class="row">
-        <div class="col-6">
-        <div class="input-group mb-3">
-            <div class="input-group-prepend">
-                <span class="input-group-text" id="inputGroup-sizing-default"  style="width: 130px; font-size: 12px;"> Modelo </span>
-            </div>
-            <input value="'.$modelo.'" type="text" id="modelo" name="modelo" class="form-control" aria-label="Default" autocomplete="off" aria-describedby="inputGroup-sizing-default" style="font-size: 14px;" >
-        </div>
-        </div>
-
-        <div class="col-6">
-        <div class="input-group mb-3">
-            <div class="input-group-prepend">
-                <span class="input-group-text" id="inputGroup-sizing-default"  style="width: 130px; font-size: 12px;"> Año </span>
-            </div>
-            <input value="'.$year.'" required type="text" id="year" name="year" class="form-control" aria-label="Default" autocomplete="off" aria-describedby="inputGroup-sizing-default" style="font-size: 14px;" >
+            <input value="'.$marca.'" required type="text" id="marca" name="marca" class="form-control" aria-label="Default" autocomplete="off" aria-describedby="inputGroup-sizing-default">
         </div>
         </div>
         </div>
 
         <div class="row">
-        <div class="col-6">
+        <div class="col-12 col-sm-6">
         <div class="input-group mb-3">
             <div class="input-group-prepend">
-                <span class="input-group-text" id="inputGroup-sizing-default"  style="width: 130px; font-size: 12px;"> Color del vechiculo </span>
+                <span class="input-group-text" id="inputGroup-sizing-default"> Modelo </span>
             </div>
-            <input value="'.$color.'" type="text" id="color" name="color" class="form-control" aria-label="Default" autocomplete="off" aria-describedby="inputGroup-sizing-default" style="font-size: 14px;" >
+            <input value="'.$modelo.'" type="text" id="modelo" name="modelo" class="form-control" aria-label="Default" autocomplete="off" aria-describedby="inputGroup-sizing-default">
         </div>
         </div>
 
-        <div class="col-6">
+        <div class="col-12 col-sm-6">
         <div class="input-group mb-3">
             <div class="input-group-prepend">
-                <span class="input-group-text" id="inputGroup-sizing-default"  style="width: 140px; font-size: 12px;"> Propietario/conductor </span>
+                <span class="input-group-text" id="inputGroup-sizing-default" > Año </span>
             </div>
-            <input value="'.$propietario.'" type="text" id="propietario" name="propietario" class="form-control" aria-label="Default" autocomplete="off" aria-describedby="inputGroup-sizing-default" style="font-size: 14px;" >
-        </div>
-        </div>
-        </div>
-
-        <div class="row">
-        <div class="col-6">
-        <div class="input-group mb-3">
-            <div class="input-group-prepend">
-                <span class="input-group-text" id="inputGroup-sizing-default"  style="width: 130px; font-size: 12px;"> Fecha ingreso </span>
-            </div>
-            <input value="'.$ingreso.'" type="date" id="ingreso" name="ingreso" class="form-control" aria-label="Default" autocomplete="off" aria-describedby="inputGroup-sizing-default" style="font-size: 14px;" >
-        </div>
-        </div>
-
-        <div class="col-6">
-        <div class="input-group mb-3">
-            <div class="input-group-prepend">
-                <span class="input-group-text" id="inputGroup-sizing-default"  style="width: 130px; font-size: 12px;"> Fecha salida </span>
-            </div>
-            <input value="'.$salida.'" type="date" id="salida" name="salida" class="form-control" aria-label="Default" autocomplete="off" aria-describedby="inputGroup-sizing-default" style="font-size: 14px;" >
+            <input value="'.$year.'" required type="text" id="year" name="year" class="form-control" aria-label="Default" autocomplete="off" aria-describedby="inputGroup-sizing-default">
         </div>
         </div>
         </div>
 
         <div class="row">
-        <div class="col-12">
+        <div class="col-12 col-sm-6">
         <div class="input-group mb-3">
             <div class="input-group-prepend">
-                <span class="input-group-text" id="inputGroup-sizing-default"  style="width: 130px; font-size: 12px;"> Observaciones </span>
+                <span class="input-group-text" id="inputGroup-sizing-default"> Color del vechiculo </span>
             </div>
-            <textarea id="observaciones" class="form-control" style="font-size: 14px;">'.$observaciones.'</textarea>
+            <input value="'.$color.'" type="text" id="color" name="color" class="form-control" aria-label="Default" autocomplete="off" aria-describedby="inputGroup-sizing-default">
+        </div>
+        </div>
+
+        <div class="col-12 col-sm-6">
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroup-sizing-default" > Propietario/conductor </span>
+            </div>
+            <input value="'.$propietario.'" type="text" id="propietario" name="propietario" class="form-control" aria-label="Default" autocomplete="off" aria-describedby="inputGroup-sizing-default">
+        </div>
+        </div>
+        </div>
+
+        <div class="row">
+        <div class="col-12 col-sm-6">
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroup-sizing-default" > Fecha ingreso </span>
+            </div>
+            <input value="'.$ingreso.'" type="date" id="ingreso" name="ingreso" class="form-control" aria-label="Default" autocomplete="off" aria-describedby="inputGroup-sizing-default" >
+        </div>
+        </div>
+
+        <div class="col-12 col-sm-6">
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroup-sizing-default" > Fecha salida </span>
+            </div>
+            <input value="'.$salida.'" type="date" id="salida" name="salida" class="form-control" aria-label="Default" autocomplete="off" aria-describedby="inputGroup-sizing-default" >
+        </div>
+        </div>
+        </div>
+
+        <div class="row">
+        <div class="col-12 col-sm-6">
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroup-sizing-default"> Observaciones </span>
+            </div>
+            <textarea id="observaciones" class="form-control">'.$observaciones.'</textarea>
         </div>
         </div>
 
