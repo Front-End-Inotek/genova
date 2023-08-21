95d896d001234764c4070040a2a0ee2ffda5567b
Carlos Garcia <carlosramongarcia@gmail.com>
2023-08-21 17:15:22 -0600

    Fix (Agregar Cliente)

--- includes/clase_huesped.php
+++ includes/clase_huesped.php
@@ -439,8 +439,9 @@
           $sentencia = "INSERT INTO `huesped` (`nombre`, `apellido`, `direccion`, `ciudad`, `estado`, `codigo_postal`, `telefono`, `correo`, `contrato`, `cupon`, `preferencias`, `comentarios`, `titular_tarjeta`,`tipo_tarjeta`, `numero_tarjeta`, `vencimiento_mes`, `vencimiento_ano`, `cvv`, `visitas`, 
           `estado_huesped`,`pais`,`empresa`,`nombre_tarjeta`,`estado_tarjeta`,`voucher`,`estado_credito`,`limite_credito`,`indole_tarjeta`)
           VALUES ('$nombre', '$apellido', '$direccion', '$ciudad', '$estado','$codigo_postal', '$telefono', '$correo', '$contrato', '$cupon', '$preferencias', '$comentarios', '$titular_tarjeta', '$tipo_tarjeta', '$numero_tarjeta', '$vencimiento_mes', '$vencimiento_ano', 
-          '$cvv', '0', '1','$pais','$empresa','$nombre_tarjeta','$estado_tarjeta','$voucher','$estado_credito','$limite_credito','$indole_tarjeta');";
+          '$cvv', '0', '1','$pais','$empresa','$nombre_tarjeta','$estado_tarjeta','$voucher','$estado_credito',$limite_credito,'$indole_tarjeta');";
           $comentario="Guardamos el huesped en la base de datos";
+          //echo $sentencia ;
           $consulta= $this->realizaConsulta($sentencia,$comentario);
           if(!$consulta){
             echo "NO_VALIDO";
