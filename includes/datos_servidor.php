6d0fd8ec2a4efe4d751d6d749541f4e10c24a679
josueG <dev_j@inotek.mx>
2023-08-23 09:38:32 -0600

    feature (plan de alimentos) se agrego el formulario y la fucionalidad de js

--- includes/datos_servidor.php
+++ includes/datos_servidor.php
@@ -2,10 +2,10 @@
   //En este archivo se almacenaran los datos del servidor para realilzar la conexion
   date_default_timezone_set('America/Mexico_City');
   $servidor="localhost";
-  //$usuario_servidor="kala";
-  //$password="ORWare1986";
-  $usuario_servidor="root";
-  $password="123";
+  $usuario_servidor="kala";
+  $password="ORWare1986";
+  //$usuario_servidor="root";
+  //$password="123";
   $base_datos="visit";
   //.gitignore
 ?>
