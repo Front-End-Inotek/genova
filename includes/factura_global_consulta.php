43eb1e6df03e31e0fce6b5a8e8a51fa4ccc846f7
josueG <dev_j@inotek.mx>
2023-10-17 09:24:19 -0600

    obtener el nombre de la habitacion con respecto a su id para mostrar en la tabla de facturacion global

--- includes/factura_global_consulta.php
+++ includes/factura_global_consulta.php
@@ -1,8 +1,10 @@
 <?php
     include_once('clase_ticket.php');
+    include_once("clase_hab.php");
     $Tickets= NEW Ticket(0);
 
     $Concepto=NEW Concepto(0);
+    $hab=NEW Hab(0);
     $fechaInicio = $_POST['fechaInio'];
     $fechaFin=$_POST['fechaFin'];
     $fechaInicioUnix=strtotime($fechaInicio);
@@ -28,6 +30,7 @@
     foreach ($lista_tickets as $fila) {
         array_push($lista_Id_tickets, $fila['id']);
         $result=$Concepto->info_concepto($fila['id']);
+        $nombre_hab=$hab->mostrar_nombre_hab($fila['id_hab']);
         //var_dump($result);
         //var_dump($result);
         if (isset($result["tipo_cargo"])){
@@ -36,13 +39,13 @@
                 array_push($lista_Nombre_Hospedaje,$result["nombre"]);
                 array_push($lista_Total_Hospedaje,$result["total"]);
                 array_push($lista_Fecha_Hospedaje,$fila['fecha']);
-                array_push($lista_IdHabitacion_Hospedaje, $fila['id_hab']);
+                array_push($lista_IdHabitacion_Hospedaje, $nombre_hab);
             }else{
                 array_push($lista_ID_Restaurante,$result["id"]);
                 array_push($lista_Nombre_Restaurante,$result["nombre"]);
                 array_push($lista_Total_Restaurante,$result["total"]);
                 array_push($lista_Fecha_Restaurante,$fila['fecha']);
-                array_push($lista_IdHabitacion_Restaurante, $fila['id_hab']);
+                array_push($lista_IdHabitacion_Restaurante, $nombre_hab);
             }
         }
     }
