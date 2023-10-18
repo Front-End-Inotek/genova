78e2bf667ca9e2f7a1ebd0185bacc1c838b2e163
josueG <dev_j@inotek.mx>
2023-10-18 17:01:26 -0600

    se corrige el error al buscar rfc

--- includes/consulta_rfc.php
+++ includes/consulta_rfc.php
@@ -5,14 +5,21 @@
     $campo = $_GET["inputRFC"];
     $nombre="";
     $email="";
+    $notas="";
+    $codigo_postal="";
+    $regimen_fiscal="";
     $resultado= $fact->consulta_rfc($campo);
-    while ($fila = mysqli_fetch_array($consulta))
+    while ($fila = mysqli_fetch_array($resultado))
       {
         $nombre=$fila["nombre"];
+        $email=$fila["email"];
+        $notas=$fila["notas"];
+        $codigo_postal=$fila["codigo_postal"];
+        $regimen_fiscal=$fila["regimen_fiscal"];
       }
     $row=mysqli_fetch_array($resultado);
     //try { count($row)>0;
-        echo $nombre.'/'.$row["email"].'/'.$row["notas"].'/'.$row["codigo_postal"].'/'.$row["regimen_fiscal"].'/'.$row["regimen_fiscal"];
+        echo $nombre.'/'.$email.'/'.$notas.'/'.$codigo_postal.'/'.$regimen_fiscal;
    // } catch (\Throwable $th) {
     //    echo "".'/'."".'/'."".'/'."".'/'."".'/'."";
     //}
