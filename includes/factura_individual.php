9e955402a9ac55f2c9a5e189b333465d8de682b6
josueG <dev_j@inotek.mx>
2023-10-18 13:16:59 -0600

    se llenan los datos de la tabla de factura individual

--- includes/factura_individual.php
+++ includes/factura_individual.php
@@ -47,12 +47,9 @@
 <!--             Imput Uso de CFDI -->
                 <div class="form-floating mb-2">
                     <select id="regimen" name ="rfc[]" class="form-select" id="inputGroupSelect01">
-                        <option selected disabled>Selecciona una opción</option>
+                        <option selected disabled>Selecciona una opciónn</option>
                         <?php
-
-                             $resulta=$fact->regimen_fiscal();
-               /*$sqal="SELECT * FROM regimen_fiscal";
-                            $resulta=mysqli_query($con,$sqal);*/
+                            $resulta=$fact->regimen_fiscal();
                             while ($vara=mysqli_fetch_array($resulta)) {  ?>
                                 <option value="<?php echo $vara[1] ?>"> <?php echo ("$vara[1] $vara[2]") ?></option>
                         <?php } ?>
@@ -64,8 +61,7 @@
                     <select id="cfdi" name ="rfc[]" class="form-select" id="inputGroupSelect01">
                         <option selected disabled>Selecciona una opción</option>
                         <?php
-                            $sqal="SELECT * FROM uso_cfdi";
-                            $resulta=mysqli_query($con,$sqal);
+                            $resulta=$fact->uso_cfdi();
                             while ($vara=mysqli_fetch_array($resulta)) {  ?>
                             <option value="<?php echo $vara[1] ?>"> <?php echo ("$vara[1] $vara[2]") ?></option>
                             <?php } ?>
@@ -82,8 +78,7 @@
                     <select id="metodopago" name="rfc[]" class="form-select" id="inputGroupSelect01">
                         <option selected disabled>Selecciona una opción</option>
                         <?php
-                            $sqal="SELECT * FROM metodo_pago";
-                            $resulta=mysqli_query($con,$sqal);
+                            $resulta=$fact->metodo_pago();
                             while ($vara=mysqli_fetch_array($resulta)) {  ?>
                                 <option value="<?php echo $vara[1] ?>"> <?php echo ("$vara[1] $vara[2]") ?></option>
                                 <?php } ?>
@@ -95,8 +90,7 @@
                     <select id="forma_pago" name="rfc[]" class="form-select" id="inputGroupSelect01">
                         <option selected disabled>Selecciona una opción</option>
                         <?php
-                            $sqal="SELECT * FROM forma_pago";
-                            $resulta=mysqli_query($con,$sqal);
+                            $resulta=$fact->forma_pago();
                             while ($vara=mysqli_fetch_array($resulta)) {  ?>
                         <option value="<?php echo $vara[1] ?>"> <?php echo ("$vara[1] $vara[2]") ?></option>
                         <?php } ?>
@@ -131,8 +125,7 @@
                             <select id="periocidad" name ="rfc[]" class="form-select" id="inputGroupSelect01">
                             <option selected disabled>Selecciona una opción</option>
                             <?php
-                                $sqal="SELECT * FROM periocidad";
-                                $resulta=mysqli_query($con,$sqal);
+                                $resulta=$fact->periocidad();
                                 while ($vara=mysqli_fetch_array($resulta)) {  ?>
                             <option value="<?php echo $vara[1] ?>"> <?php echo ("$vara[1] $vara[2]") ?></option>
                             <?php } ?>
@@ -144,8 +137,7 @@
                         <select id="mes" name ="rfc[]" class="form-select" id="inputGroupSelect01" >
                             <option selected disabled>Selecciona una opción</option>
                             <?php
-                                $sqal="SELECT * FROM mes";
-                                $resulta=mysqli_query($con,$sqal);
+                                $resulta=$fact->mes();
                                 while ($vara=mysqli_fetch_array($resulta)) {  ?>
                             <option value="<?php echo $vara[1] ?>"> <?php echo ("$vara[1] $vara[2]") ?></option>
                             <?php } ?>
