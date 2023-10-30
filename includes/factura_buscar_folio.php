1edc4de02897c368a17d7cc45bc3cd55a59949e6
Front-end Developer <dev_e@inotek.mx>
2023-10-30 09:21:30 -0600

    se corrigieron estilos para las facturas

--- includes/factura_buscar_folio.php
+++ includes/factura_buscar_folio.php
@@ -2,16 +2,16 @@
 
 echo '
     <div class="container-fluid blanco">
-        <div class="col-12 text-center"><h2 class="text-dark">Buscar factura por fecha</h2></div>
+        <div class="col-12 text-center"><h2 class="text-dark">Buscar factura por folio</h2></div>
         <br>
         <div class="row">
             <div class="col-sm-2 form-floating mb-3">
-                <input type="number" class="form-control" id="inicial" placeholder="Factura inicial" style="padding-left: 25px;">
-                <label for="inicial">Folio inicial de la Factura</label>
+                <input type="number" class="form-control" id="inicial" placeholder="Factura inicial">
+                <label for="inicial" style="padding-left: 25px;" >Folio inicial de la Factura</label>
             </div>
             <div class="col-sm-2 form-floating mb-3">
-                <input type="number" class="form-control" id="final" placeholder="Factura final" style="padding-left: 25px;">
-                <label for="final">Folio final de la factura</label>
+                <input type="number" class="form-control" id="final" placeholder="Factura final">
+                <label for="final" style="padding-left: 25px;" >Folio final de la factura</label>
             </div>
             <div class="col-sm-2">
                 <button class="btn btn-primary btn-block btn-default" onclick="buscar_factura_folio()" > Buscar </button>
