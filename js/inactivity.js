00e8075d04f231d26e85a688dff6b5aa189a572d
back-end <dev_j@inotek.mx>
2023-12-01 11:15:17 -0600

    cerrar sesion

--- js/inactivity.js
+++ js/inactivity.js
@@ -5,10 +5,10 @@ let timerInterval;
 const startTimer = () => {
     setInterval(() => {
         inactivityTime += 1;
-        console.log(inactivityTime);
-        if (inactivityTime >= 30) {
-            alert("Desconexión por inactividad");
+        //console.log(inactivityTime);
+        if (inactivityTime >= 3000) {
             clearInterval(timerInterval);
+            salirsession()
         }
     }, 1000);
 
@@ -16,8 +16,36 @@ const startTimer = () => {
 }
 
 const resetTimer = () => {
-    console.log("Reseteo del invernadero");
+    //console.log("Reseteo del invernadero");
     inactivityTime = 0;
+}
+// Salir de la session
+function salirsession(){
+	let usuario_id = localStorage.getItem("id");
+    localStorage.removeItem('id');
+    localStorage.removeItem('tocken');
+    localStorage.removeItem('vista');
+    localStorage.removeItem('txt_vista');
+    //remover el token de la db?
+    include="includes/remover_token.php?usuario="+usuario_id
+    $.ajax({
+        async:true,
+        type: "GET",
+        dataType: "HTML",
+        contentType: "application/json",
+        url:include,
+        beforeSend:loaderbar,
+        //una vez eliminado el token de la bd, se redirecciona.
+        success:function(res){
+            document.location.href='index.php';
+        },
+        //success:problemas_sistema,
+        timeout:5000,
+        error:function(err){
+            console.log(err)
+            swal("Error del servidor!", "Intenelo de nuevo o contacte con soporte tecnico", "error");
+        }
+    });
 }
 
 document.addEventListener("mousemove", resetTimer);
