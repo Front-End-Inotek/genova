43b1720f13ee260580852e5625f7ba4ac468fb8f
Front-end Developer <dev_e@inotek.mx>
2023-12-06 11:49:24 -0600

    correcion de estilos en reporte de reservaciones

--- includes/ver_reporte_reservacion.php
+++ includes/ver_reporte_reservacion.php
@@ -7,36 +7,43 @@
   $ruta = $_GET['ruta'];
   $titulo = $_GET['titulo'];
 
-  echo ' <div class="container blanco">
-         <div class="row">
-         <div class="col-sm-11 blanco_margen">
+  echo ' <div class="main_container">
 
-            <div class="row">
+            <header class="main_container_title">
 
-              <div class="col-sm-7 d-flex justify-content-end "><h2 class="text-dark">'.$titulo.' '.$_GET['id'].'</h2></div>
+              <h2>'.$titulo.' '.$_GET['id'].'</h2>
 
-              <div class="col-sm-4  ">
-              <div class=" d-flex justify-content-between">
-                <div  id="boton_reservacion">
-                  <button type="submit" class="btn btn-success btn-block margen-1" value="Imprimir" onclick="reporte_reservacion('.$_GET['id'].',\''.$titulo.'\')">Imprimir</button>
-                </div>';
-                if($titulo=="RESERVACIÓN"){
-                  echo '<div  id="boton_reservacion">
-                  <button type="submit" class="btn btn-success btn-block margen-1" value="Reenviar" onclick="enviar_reserva_correo('.$_GET['id'].',\''.$correo.'\',true)">Reenviar</button>
+              <div class="btn_resumen_reservacion">
+                  <div  id="boton_reservacion">
+                    <button type="submit" class="btn btn-primary" value="Imprimir" onclick="reporte_reservacion('.$_GET['id'].',\''.$titulo.'\')">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-pdf" viewBox="0 0 16 16">
+                        <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
+                        <path d="M4.603 14.087a.81.81 0 0 1-.438-.42c-.195-.388-.13-.776.08-1.102.198-.307.526-.568.897-.787a7.68 7.68 0 0 1 1.482-.645 19.697 19.697 0 0 0 1.062-2.227 7.269 7.269 0 0 1-.43-1.295c-.086-.4-.119-.796-.046-1.136.075-.354.274-.672.65-.823.192-.077.4-.12.602-.077a.7.7 0 0 1 .477.365c.088.164.12.356.127.538.007.188-.012.396-.047.614-.084.51-.27 1.134-.52 1.794a10.954 10.954 0 0 0 .98 1.686 5.753 5.753 0 0 1 1.334.05c.364.066.734.195.96.465.12.144.193.32.2.518.007.192-.047.382-.138.563a1.04 1.04 0 0 1-.354.416.856.856 0 0 1-.51.138c-.331-.014-.654-.196-.933-.417a5.712 5.712 0 0 1-.911-.95 11.651 11.651 0 0 0-1.997.406 11.307 11.307 0 0 1-1.02 1.51c-.292.35-.609.656-.927.787a.793.793 0 0 1-.58.029zm1.379-1.901c-.166.076-.32.156-.459.238-.328.194-.541.383-.647.547-.094.145-.096.25-.04.361.01.022.02.036.026.044a.266.266 0 0 0 .035-.012c.137-.056.355-.235.635-.572a8.18 8.18 0 0 0 .45-.606zm1.64-1.33a12.71 12.71 0 0 1 1.01-.193 11.744 11.744 0 0 1-.51-.858 20.801 20.801 0 0 1-.5 1.05zm2.446.45c.15.163.296.3.435.41.24.19.407.253.498.256a.107.107 0 0 0 .07-.015.307.307 0 0 0 .094-.125.436.436 0 0 0 .059-.2.095.095 0 0 0-.026-.063c-.052-.062-.2-.152-.518-.209a3.876 3.876 0 0 0-.612-.053zM8.078 7.8a6.7 6.7 0 0 0 .2-.828c.031-.188.043-.343.038-.465a.613.613 0 0 0-.032-.198.517.517 0 0 0-.145.04c-.087.035-.158.106-.196.283-.04.192-.03.469.046.822.024.111.054.227.09.346z"/>
+                      </svg>
+                      Imprimir
+                    </button>
                   </div>';
-                }
-                //<div ><button class="btn btn-info btn-block" onclick="'.$ruta.'">⬅</button></div>
-                echo '
-                <div >
-                  <button class="btn btn-info btn-block" onclick="'.$ruta.'">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
-                      <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
-                    </svg>
-                  </button>
-                </div>
+                  if($titulo=="RESERVACIÓN"){
+                    echo '
+                    <div  id="boton_reservacion">
+                    <button type="submit" class="btn btn-primary" value="Reenviar" onclick="enviar_reserva_correo('.$_GET['id'].',\''.$correo.'\',true)">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-pdf" viewBox="0 0 16 16">
+                        <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
+                        <path d="M4.603 14.087a.81.81 0 0 1-.438-.42c-.195-.388-.13-.776.08-1.102.198-.307.526-.568.897-.787a7.68 7.68 0 0 1 1.482-.645 19.697 19.697 0 0 0 1.062-2.227 7.269 7.269 0 0 1-.43-1.295c-.086-.4-.119-.796-.046-1.136.075-.354.274-.672.65-.823.192-.077.4-.12.602-.077a.7.7 0 0 1 .477.365c.088.164.12.356.127.538.007.188-.012.396-.047.614-.084.51-.27 1.134-.52 1.794a10.954 10.954 0 0 0 .98 1.686 5.753 5.753 0 0 1 1.334.05c.364.066.734.195.96.465.12.144.193.32.2.518.007.192-.047.382-.138.563a1.04 1.04 0 0 1-.354.416.856.856 0 0 1-.51.138c-.331-.014-.654-.196-.933-.417a5.712 5.712 0 0 1-.911-.95 11.651 11.651 0 0 0-1.997.406 11.307 11.307 0 0 1-1.02 1.51c-.292.35-.609.656-.927.787a.793.793 0 0 1-.58.029zm1.379-1.901c-.166.076-.32.156-.459.238-.328.194-.541.383-.647.547-.094.145-.096.25-.04.361.01.022.02.036.026.044a.266.266 0 0 0 .035-.012c.137-.056.355-.235.635-.572a8.18 8.18 0 0 0 .45-.606zm1.64-1.33a12.71 12.71 0 0 1 1.01-.193 11.744 11.744 0 0 1-.51-.858 20.801 20.801 0 0 1-.5 1.05zm2.446.45c.15.163.296.3.435.41.24.19.407.253.498.256a.107.107 0 0 0 .07-.015.307.307 0 0 0 .094-.125.436.436 0 0 0 .059-.2.095.095 0 0 0-.026-.063c-.052-.062-.2-.152-.518-.209a3.876 3.876 0 0 0-.612-.053zM8.078 7.8a6.7 6.7 0 0 0 .2-.828c.031-.188.043-.343.038-.465a.613.613 0 0 0-.032-.198.517.517 0 0 0-.145.04c-.087.035-.158.106-.196.283-.04.192-.03.469.046.822.024.111.054.227.09.346z"/>
+                      </svg>
+                      Reenviar
+                    </button>
+                    </div>';
+                  }
+                  //<div ><button class="btn btn-info btn-block" onclick="'.$ruta.'">⬅</button></div>
+                  echo '
+                    <button class="btn btn-link" onclick="'.$ruta.'">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-square-fill" viewBox="0 0 16 16">
+                        <path d="M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2zm-4.5-6.5H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5a.5.5 0 0 0 0-1"></path>
+                      </svg>
+                    </button>
               </div>
-              </div>
-            </div>';
+            </header>';
             $consulta= $reservacion->datos_reservacion($_GET['id']);
             while ($fila = mysqli_fetch_array($consulta))
             {
@@ -98,8 +105,8 @@
             }
             $huesped= NEW Huesped($id_huesped);
             echo '
-            <div class="col-12 row">
-            <ul class="list-group list-group-flush col-6">
+            <div class="contenedor_reporte_reservacion">
+            <ul class="list-group list-group-flush list_group_perzonalizado " >
               <li class="list-group-item">
                 <h5 class="texto_reporte ">Fecha Entrada:</h5>
                 <h4>'.($fecha_entrada ? $fecha_entrada : "-").'</h4>
@@ -144,45 +151,39 @@
               }
               echo '
               </ul>
-
-              <ul class="list-group list-group-flush col-6">
-              <li class="list-group-item">
-              <h5 class="texto_reporte ">Fecha Salida:</h5>
-                <h4>'.($fecha_salida ? $fecha_salida : "-" ).'</h4>
-                </li>
-              <li class="list-group-item">
-              <h5 class="texto_reporte ">Cantidad habitaciones</h5>
-                <h4>'.($numero_hab ? $numero_hab : "-").'</h4>
-              </li>
-              <li class="list-group-item">
-                <h5 class="texto_reporte ">Nombre Tarifa:</h5>
-                <h4>'.($nombre_tarifa ? $nombre_tarifa : "-" ).'</h4>
-              </li>
-              <li class="list-group-item">';
-                if($quien_reserva  != "checkin"){
-                  echo '
-                    <h5 class="texto_reporte ">Quién Reserva:</h5>
-                    <h4>'.$quien_reserva.'</h4>
-                  ';
-                }else {
-                  echo '
-                  ';
-                }
-                echo '
+             
+              <ul class="list-group list-group-flush list_group_perzonalizado ">
+                <li class="list-group-item">
+                <h5 class="texto_reporte ">Fecha Salida:</h5>
+                  <h4>'.($fecha_salida ? $fecha_salida : "-" ).'</h4>
+                  </li>
+                <li class="list-group-item">
+                <h5 class="texto_reporte ">Cantidad habitaciones</h5>
+                  <h4>'.($numero_hab ? $numero_hab : "-").'</h4>
                 </li>
                 <li class="list-group-item">
-                <h5 class="texto_reporte">Cantidad Hospedaje:</h5> 
-                <h4>'.($cantidad_hospedaje ? $cantidad_hospedaje : "-" ).'</h4>
+                  <h5 class="texto_reporte ">Nombre Tarifa:</h5>
+                  <h4>'.($nombre_tarifa ? $nombre_tarifa : "-" ).'</h4>
+                </li>
+                <li class="list-group-item">';
+                  if($quien_reserva  != "checkin"){
+                    echo '
+                      <h5 class="texto_reporte ">Quién Reserva:</h5>
+                      <h4>'.$quien_reserva.'</h4>
+                    ';
+                  }else {
+                    echo '
+                    ';
+                  }
+                  echo '
+                </li>
+                <li class="list-group-item">
+                  <h5 class="texto_reporte">Cantidad Hospedaje:</h5>
+                  <h4>'.($cantidad_hospedaje ? $cantidad_hospedaje : "-" ).'</h4>
                 </li>
               </ul>
-            </div>
-
-
-            <!-- -------------------------------------------------------------------------------- -->
-
-
-            <div class="col-12 row">
-              <ul class="list-group list-group-flush col-6">
+ 
+              <ul class="list-group list-group-flush list_group_perzonalizado ">
                 <li class="list-group-item">
                   <h5 class="texto_reporte ">Número Cuenta:</h5>
                   <h4>'.($id_cuenta ? $id_cuenta : "-").'</h4>
@@ -199,12 +200,13 @@
                   <h5 class="texto_reporte ">Total Habitación:</h5>
                   <h4>'.($total_habitacion ? $total_habitacion : "-").'</h4>
                 </li>
-                <li>
+                <li class="list-group-item">
                   <h5 class="texto_reporte ">Total Estancia:</h5>
                   <h4>'.($total_estancia ? $total_estancia : "-" ).'</h4>
                 </li>
               </ul>
-              <ul class="list-group list-group-flush col-6">
+
+              <ul class="list-group list-group-flush list_group_perzonalizado ">
                 <li class="list-group-item">
                   <h5 class="texto_reporte ">Precio Hospedaje:</h5>
                   <h4>'.($precio_hospedaje ? $precio_hospedaje : "-" ).'</h4>
@@ -228,12 +230,12 @@
               </ul>
             </div>
 
-            <div class="row">
-              <div class="col-sm-12 text-left"><h4 class="text-dark">DATOS HUÉSPED</h4></div>
+            <div class="main_container_title">
+              <h2>DATOS HUÉSPED</h2>
             </div>
 
-            <div class="col-12 row">
-                <ul class="list-group list-group-flush col-6">
+            <div class="contenedor_reporte_reservacion">
+                <ul class="list-group list-group-flush list_group_perzonalizado ">
                   <li class="list-group-item">
                     <h5 class="texto_reporte" >Nombre</h5>
                     <h4>'.($huesped->nombre ? $huesped->nombre : "-").'</h4>
@@ -254,12 +256,13 @@
                     <h5 class="texto_reporte">Contrato Socio:</h5>
                     <h4>'.($huesped->contrato ? $huesped->contrato : "-").'</h4>
                   </li>
-                  <li>
+                  <li class="list-group-item">
                     <h5 class="texto_reporte">Preferencias del huésped</h5>
                     <h4>'.($huesped->preferencias ? $huesped->preferencias : "-").'</h4>
                   </li>
                 </ul>
-                <ul class="list-group list-group-flush col-6">
+
+                <ul class="list-group list-group-flush list_group_perzonalizado ">
                   <li class="list-group-item">
                     <h5 class="texto_reporte">Apellido:</h5>
                     <h4>'.($huesped->apellido ? $huesped->apellido : "-").'</h4>
@@ -286,8 +289,6 @@
                   </li>
                 </ul>
             </div>
-        </div>';
-  echo '
-         </div><div class="col-sm-1"></div>
-         </div></div>';
+          </div>
+            ';
 ?>
