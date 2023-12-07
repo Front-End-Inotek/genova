b8daab2cb258081e150239468cb9c80f7fe344ec
Front-end Developer <dev_e@inotek.mx>
2023-12-07 11:22:51 -0600

    correcion de estilos modal de cancelacion

--- includes/cancelar_modal_reservacion.php
+++ includes/cancelar_modal_reservacion.php
@@ -14,32 +14,37 @@
   <!-- Modal content-->
   <div class="modal-content">
     <div class="modal-header">
-      Cancelar Reservación: '.$_GET['id']; 
-      echo '<button type="button" class="close" data-dismiss="modal">&times;</button>
-    </div><br>
+      <h5 class="modal-title">Cancelar Reservación: '.$_GET['id']  ; echo '</h5>
+      <button type="button" class="btn btn-light" data-dismiss="modal">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+          <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"></path>
+        </svg>
+      </button>
+    </div>
 
     <div class="modal-body">
-      <div class="row">
-        <div class="col-sm-3" >Quién Cancela:</div>
-        <div class="col-sm-9" >
-        <div class="form-group">
-          <input class="form-control" type="text"  id="nombre_cancela" placeholder="Ingresa el nombre de quién cancela" maxlength="60">
-        </div>
-        </div>
+
+    <div class="inputs_form_container" >
+      <div class="form-floating input_container">
+        <input type="text" name="nombre_cancela" class="form-control custom_input" id="nombre_cancela" maxlength="60" placeholder="Numero de tarjeta">
+        <label class="asterisco" for="nombre_cancela">Quién cancela</label>
       </div>
-      <div class="row">
-        <div class="col-sm-3" >Motivo de cancelación:</div>
-        <div class="col-sm-9" >
-        <div class="form-group">
-          <textarea class="form-control" id="motivo_cancela" placeholder="Ingresa el motivo/razón por la que cancela la reservación" maxlength="255"></textarea>
-        </div>
-        </div>
-      </div><br>
+    </div>
+
+
+    <div class="inputs_form_container" >
+      <div class="form-floating input_container">
+        <input type="text" name="nombre_cancela" class="form-control custom_input" id="motivo_cancela" maxlength="255" placeholder="Numero de tarjeta">
+        <label class="asterisco" for="motivo_cancela">Motivo de cancelacíon</label>
+      </div>
+    </div>
+
+    
     </div>
     
     <div class="modal-footer" id="boton_cancelar_reservacion">
       <button type="button" class="btn btn-danger" data-dismiss="modal"> Cancelar</button>
-      <button type="button" class="btn btn-success" onclick="cancelar_reservacion('.$_GET['id'].','.$preasignada.',\''.$correo.'\','.$garantizada.')"> Aceptar</button>
+      <button type="button" class="btn btn-primary" onclick="cancelar_reservacion('.$_GET['id'].','.$preasignada.',\''.$correo.'\','.$garantizada.')"> Aceptar</button>
     </div>
   </div>';
 ?>
