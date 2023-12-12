648673bd50484a41f3b96d72984f5cde45d401d6
Front-end Developer <dev_e@inotek.mx>
2023-12-12 10:25:50 -0600

    se cambiaron los botones para cerrar el modal

--- includes/hab_modal_sucia_hospedaje.php
+++ includes/hab_modal_sucia_hospedaje.php
@@ -6,14 +6,18 @@
   <!-- Modal content-->
   <div class="modal-content">
     <div class="modal-header">
-      <button type="button" class="close" data-dismiss="modal">&times;</button>
-    </div><br>
+      <button type="button" class="btn btn-light" data-dismiss="modal">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+          <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"></path>
+        </svg>
+      </button>
+    </div>
     <div class="modal-body">
         ¿Poner como sucia a la habitación '.$hab->nombre.'?
     </div><br>
     <div class="modal-footer">
       <button type="button" class="btn btn-danger" data-dismiss="modal"> Cancelar</button>
-      <button type="button" class="btn btn-success" onclick="hab_ocupada_sucia('.$_GET['hab_id'].','.$_GET['estado'].')"> Aceptar</button>
+      <button type="button" class="btn btn-primary" onclick="hab_ocupada_sucia('.$_GET['hab_id'].','.$_GET['estado'].')"> Aceptar</button>
     </div>
   </div>';
 ?>
--- old/includes/hab_modal_disponible_hospedaje.php
+++ old/includes/hab_modal_disponible_hospedaje.php
@@ -6,7 +6,11 @@
   <!-- Modal content-->
   <div class="modal-content">
     <div class="modal-header">
-      <button type="button" class="close" data-dismiss="modal">&times;</button>
+      <button type="button" class="btn btn-light" data-dismiss="modal">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+          <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"></path>
+        </svg>
+      </button>
     </div><br>
 
     <div class="modal-body">
@@ -15,7 +19,7 @@
     
     <div class="modal-footer">
       <button type="button" class="btn btn-danger" data-dismiss="modal"> Cancelar</button>
-      <button type="button" class="btn btn-success" onclick="hab_disponible_sucia('.$_GET['hab_id'].','.$_GET['estado'].')"> Aceptarr</button>
+      <button type="button" class="btn btn-primary" onclick="hab_disponible_sucia('.$_GET['hab_id'].','.$_GET['estado'].')"> Aceptarr</button>
     </div>
   </div>';
 ?>
