75746c52fe02ab47ad8e923974ec1d1bc9a2152c
Front end <dev_e@inotek.mx>
2023-12-14 11:01:51 -0600

    encabezado de la tabla

--- includes/asignar_reservaciones.php
+++ includes/asignar_reservaciones.php
@@ -8,6 +8,30 @@
             <h2>Asignar habitaciones a la reserva '.$numero_reserva.' </h2>
         </div>
 
+        <div class="table-responsive">
+            <table class="table table-hover table-expansion">
+                <thead>
+                    <tr class=" text-center">
+                        <th>Número</th>
+                        <th>Fecha Entrada</th>
+                        <th>Fecha Salida</th>
+                        <th>Nombre Huésped</th>
+                        <th>Noches</th>
+                        <th>Tarifa</th>
+                        <th>Precio Hospedaje</th>
+                        <th>Plan alimentos</th>
+                        <th>Extra Adulto</th>
+                        <th>Extra Menor</th>
+                        <th>Total Estancia</th>
+                        <th>Total Pago</th>
+                        <th>Forma Pago</th>
+                        <th>Preasugnar</th>
+                        <th>Herramientas</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+
 
     </div>
     '
