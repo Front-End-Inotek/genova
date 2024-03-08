2b9b1fe1fd5b5f4a1e5e63163f3fe853ace53141
back_dev <back@inotek.mx>
2024-03-08 10:24:49 -0600

    se agrega un campo de fecha de facturacion para mostrar los tickets facturados tal dia

--- includes/clase_ticket.php
+++ includes/clase_ticket.php
@@ -154,18 +154,19 @@
       function reporte_tickets_en_rango_fechas_facturado($inicio,$fin){
         $sentencia="SELECT *, ticket.id AS id_ticket, ticket.tiempo AS ticket_fecha FROM ticket
         LEFT JOIN facturas ON ticket.id_factura = facturas.id
-        WHERE ticket.facturado=1  AND ticket.tiempo>=$inicio AND ticket.tiempo<=$fin
+        WHERE ticket.facturado=1  AND ticket.fecha_facturacion>=$inicio AND ticket.fecha_facturacion<=$fin
         UNION
         SELECT *, ticket.id, ticket.tiempo FROM ticket
         RIGHT JOIN facturas ON ticket.id_factura = facturas.id
-        WHERE ticket.facturado=1  AND ticket.tiempo>=$inicio AND ticket.tiempo<=$fin ;";
+        WHERE ticket.facturado=1  AND ticket.fecha_facturacion>=$inicio AND ticket.fecha_facturacion<=$fin ;";
         //echo $sentencia;
         $comentario="obtener tickets en rango de fechas para llenar reporte";
         $consulta= $this->realizaConsulta($sentencia,$comentario);
         return $consulta;
       }
       function cambiar_estado_facturados($id,$id_factura){
-        $sentencia="UPDATE ticket SET facturado = 1, id_factura = '$id_factura'  WHERE id = $id";
+        $tiempo_unix=time();
+        $sentencia="UPDATE ticket SET facturado = 1, id_factura = '$id_factura' ,fecha_facturacion='$tiempo_unix'  WHERE id = $id";
         //echo $sentencia;
         $comentario="cambio de estado en la columna de acturado";
         $consulta= $this->realizaConsulta($sentencia,$comentario);
