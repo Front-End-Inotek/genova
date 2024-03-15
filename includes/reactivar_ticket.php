c7178d9e6d69f1ff7a23188561657bb6f78bce07
Front end <dev_e@inotek.mx>
2024-03-15 15:37:21 -0600

    correciones

--- includes/reactivar_ticket.php
+++ includes/reactivar_ticket.php
@@ -1,7 +1,7 @@
 <?php
 include_once("clase_ticket.php");
 $ticket= NEW Ticket(0);
-$id_ticket= $_POST("id_ticket");
+$id_ticket= $_POST["id_ticket"];
 $ticket->cambiar_no_facturado($id_ticket);
 
 echo 'SI';
