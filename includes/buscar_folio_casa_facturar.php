d15c819c7bcdcc16e6fa22cca8902fbc0d8c5190
Front end <dev_e@inotek.mx>
2024-03-15 16:59:27 -0600

    a ver

--- includes/buscar_folio_casa_facturar.php
+++ includes/buscar_folio_casa_facturar.php
@@ -98,6 +98,9 @@
             <div class="ticket_container_header_input ">
               <input type="checkbox" id="leer_check_'.$contadoriteraciones.'" disabled/>
               <input class="d-none" type="number" id="leer_hab" value='.$hab->mostrar_nombre_hab($fila["id_hab"]).'>
+              <button class="btn btn-primary btn_reactivar" onclick="cambiarNoFac2('.$fila['id'].')">
+                Reactivar
+              </button>
             </div>
             <div class="ticket_container_header_info">
               <div class="ticket_container_header_info_fecha">
