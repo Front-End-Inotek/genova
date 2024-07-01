14de2228e31d0087efbdea418e3c8113e98e80a6
Front end <dev_e@inotek.mx>
2024-07-01 15:35:18 -0600

    carrusel agregado

--- src/includes/agregar_reservacion.php
+++ src/includes/agregar_reservacion.php
@@ -173,6 +173,34 @@ $end = $_GET["end"];
         </form>
       </div>
       <div class="col-md-7 col-lg-8">
+
+      <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="https://images.pexels.com/photos/26781807/pexels-photo-26781807/free-photo-of-llama.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="d-block w-100 img-fluid" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="https://images.pexels.com/photos/26741435/pexels-photo-26741435/free-photo-of-mar-playa-gente-arena.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="d-block w-100 img-fluid" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="https://images.pexels.com/photos/26741435/pexels-photo-26741435/free-photo-of-mar-playa-gente-arena.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="d-block w-100 img-fluid" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+
         <h4 class="mb-3">Detalles de la rervacion</h4>
         <form class="needs-validation" novalidate>
           <div class="row g-3">
